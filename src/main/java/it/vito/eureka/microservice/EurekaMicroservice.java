369d12bc0f478ec3ad7d870db2f3e641cde1a30f
Przemurb <przemurb@hotmail.it>
2022-12-11 16:18:31 +0100

    Initial commit

---
+++ src/main/java/it/vito/eureka/microservice/EurekaMicroservice.java
@@ -0,0 +1,13 @@
+package it.vito.eureka.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaMicroservice {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaMicroservice.class, args);
+	}
+
+}
---
+++ src/test/java/it/vito/eureka/microservice/EurekaMicroserviceTests.java
@@ -0,0 +1,13 @@
+package it.vito.eureka.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaMicroserviceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
