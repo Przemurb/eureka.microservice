65afd6c3aaae94fcd9a143a8cba1636501e0d975
Przemurb <przemurb@hotmail.it>
2022-12-11 20:44:08 +0100

    Mikroservis i jego konfiguracja

--- src/main/java/it/vito/eureka/microservice/EurekaMicroservice.java
+++ src/main/java/it/vito/eureka/microservice/EurekaMicroservice.java
@@ -1,9 +1,13 @@
 package it.vito.eureka.microservice;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EurekaMicroservice {
 
 	public static void main(String[] args) {
